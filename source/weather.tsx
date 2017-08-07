aa85c62a2b252ba9ffafdf75589be39e90e36fe4
Pedro Ferreira <plferreira4@gmail.com>
2017-08-07 18:48:07 +0100

    - add the description of each property off the 'WeatherInfo' interface - add some missing properties to the 'WeatherInfo'  interface, and display them as well - use metric units for the temperature/etc

--- source/weather.tsx
+++ source/weather.tsx
@@ -1,25 +1,29 @@
 import * as React from "react";
 
+    // reference: http://openweathermap.org/current
 interface WeatherInfo {
-    name: string;
+    name: string;               // City name
     weather: {
-        description: string;
+        main: string;           // Group of weather parameters (Rain, Snow, Extreme etc.)
+        description: string;    // Weather condition within the group
     }[];
     coord: {
-        lat: number;
-        lon: number;
+        lat: number;            // City geo location, latitude
+        lon: number;            // City geo location, longitude
     };
     main: {
-        humidity: number;
-        pressure: number;
-        temp: number;
-        temp_min: number;
-        temp_max: number;
+        humidity: number;       // Humidity, %
+        pressure: number;       // Atmospheric pressure (on the sea level, if there is no sea_level or grnd_level data), hPa
+        temp: number;           // Temperature. Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+        temp_min: number;       // Minimum temperature at the moment. This is deviation from current temp that is possible for large cities and megalopolises geographically expanded (use these parameter optionally). Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+        temp_max: number;       // Maximum temperature at the moment. This is deviation from current temp that is possible for large cities and megalopolises geographically expanded (use these parameter optionally). Unit Default: Kelvin, Metric: Celsius, Imperial: Fahrenheit.
+        sea_level: number;      // Atmospheric pressure on the sea level, hPa
+        grnd_level: number;     // Atmospheric pressure on the ground level, hPa
     };
     wind: {
-        speed: number;
-        deg: number;
-    }
+        speed: number;          // Wind speed. Unit Default: meter/sec, Metric: meter/sec, Imperial: miles/hour.
+        deg: number;            // Wind direction, degrees (meteorological)
+    };
 }
 
 interface WeatherProps {
@@ -45,17 +49,17 @@ class Weather extends React.Component <WeatherProps, WeatherState> {
         switch( event.which ) {
             case 13:    // enter
                 let input = event.target as HTMLInputElement;
-                this.getWeather( input.value );
+                this.getCurrentWeather( input.value );
                 break;
         }
     }
 
-    async getWeather( name: string ) {
+    async getCurrentWeather( cityName: string ) {
 
-        var response = await fetch( `http://api.openweathermap.org/data/2.5/weather?q=${ name }&appid=8cffe81fbe82ac71521e0cf28f0f3496` );
+        var response = await fetch( `http://api.openweathermap.org/data/2.5/weather?q=${ cityName }&appid=8cffe81fbe82ac71521e0cf28f0f3496&units=metric` );
 
         if ( response.status !== 200 ) {
-            console.warn( 'Name:', name );
+            console.warn( 'Name:', cityName );
             return;
         }
 
@@ -77,19 +81,23 @@ class Weather extends React.Component <WeatherProps, WeatherState> {
                 let weather = info.weather[ a ];
 
                 weatherInfo.push(
-                    <div key= { a }>Description: { weather.description }</div>
+                    <div key= { a }>
+                        Main: { weather.main } Description: { weather.description }
+                    </div>
                 );
             }
 
             current = (
                 <div>
                     <div>Latitude: { info.coord.lat } / Longitude: { info.coord.lon }</div>
-                    <div>Humidity: { info.main.humidity }</div>
-                    <div>Pressure: { info.main.pressure }</div>
-                    <div>Temperature: { info.main.temp }</div>
-                    <div>Min Temperature: { info.main.temp_min }</div>
-                    <div>Max Temperature: { info.main.temp_max }</div>
-                    <div>Wind Speed: { info.wind.speed } / Degree: { info.wind.deg }</div>
+                    <div>Humidity: { info.main.humidity } %</div>
+                    <div>Pressure: { info.main.pressure } hPa</div>
+                    <div>Temperature: { info.main.temp } °C</div>
+                    <div>Min Temperature: { info.main.temp_min } °C</div>
+                    <div>Max Temperature: { info.main.temp_max } °C</div>
+                    <div>Atmospheric pressure on the sea level: { info.main.sea_level } hPa</div>
+                    <div>Atmospheric pressure on the ground level: { info.main.grnd_level } hPa</div>
+                    <div>Wind Speed: { info.wind.speed } meter/sec / Degree: { info.wind.deg } °</div>
                     <div>{ weatherInfo }</div>
                 </div>
             );
