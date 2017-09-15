061db1bf696a5071ac86dc9c564eee266803cc89
Pedro Ferreira <plferreira4@gmail.com>
2017-09-15 12:36:20 +0100

    - update the build script to be able to build a compressed version of the bundle file - use the 'uglify-es' minimizer to work with es2015+ code

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,48 @@
-module.exports = {
-    entry: "./source/index.tsx",
-    output: {
-        filename: "bundle.js",
-        path: __dirname + "/dist"
-    },
+const UglifyJSPlugin = require( 'uglifyjs-webpack-plugin' );
 
-    // Enable sourcemaps for debugging webpack's output.
-    devtool: "source-map",
 
-    resolve: {
-        // Add '.ts' and '.tsx' as resolvable extensions.
-        extensions: [".ts", ".tsx", ".js", ".json"]
-    },
+module.exports = function( env ) {
 
-    module: {
-        rules: [
-            // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
-            { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+    let plugins = [];
 
-            // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-            { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-        ]
-    },
+    if ( env && env.release ) {
+        plugins.push( new UglifyJSPlugin() );
+    }
 
-    // When importing a module whose path matches one of the following, just
-    // assume a corresponding global variable exists and use that instead.
-    // This is important because it allows us to avoid bundling all of our
-    // dependencies, which allows browsers to cache those libraries between builds.
-    externals: {
-        "react": "React",
-        "react-dom": "ReactDOM"
-    },
+    return {
+        entry: "./source/index.tsx",
+        output: {
+            filename: "bundle.js",
+            path: __dirname + "/dist"
+        },
+
+        plugins: plugins,
+
+        // Enable sourcemaps for debugging webpack's output.
+        devtool: "source-map",
+
+        resolve: {
+            // Add '.ts' and '.tsx' as resolvable extensions.
+            extensions: [".ts", ".tsx", ".js", ".json"]
+        },
+
+        module: {
+            rules: [
+                // All files with a '.ts' or '.tsx' extension will be handled by 'awesome-typescript-loader'.
+                { test: /\.tsx?$/, loader: "awesome-typescript-loader" },
+
+                // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
+                { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+            ]
+        },
+
+        // When importing a module whose path matches one of the following, just
+        // assume a corresponding global variable exists and use that instead.
+        // This is important because it allows us to avoid bundling all of our
+        // dependencies, which allows browsers to cache those libraries between builds.
+        externals: {
+            "react": "React",
+            "react-dom": "ReactDOM"
+        },
+    };
 };
