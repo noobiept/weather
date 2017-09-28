5c4b94c99449ec18b778c6207e3b0f40a6701d1f
Pedro Ferreira <plferreira4@gmail.com>
2017-09-28 17:02:52 +0100

    - fix the build script, to work with the new react version

--- build.sh
+++ build.sh
@@ -14,11 +14,10 @@ echo "\nCopy the necessary files.\n"
 cp -v ./index.html ./release/index.html
 cp -v ./dist/bundle.js ./release/bundle.js
 cp -v -a ./css/ ./release/
-cp -v ./node_modules/react/dist/react.min.js ./release/libraries/react.js
-cp -v ./node_modules/react-dom/dist/react-dom.min.js ./release/libraries/react-dom.js
+cp -v ./node_modules/react/umd/react.production.min.js ./release/libraries/react.production.min.js
+cp -v ./node_modules/react-dom/umd/react-dom.production.min.js ./release/libraries/react-dom.production.min.js
 
 echo "\nUpdate the index.html paths.\n"
-
 htmlprocessor ./release/index.html -o ./release/index.html
 
 echo "\nDone!\n"
