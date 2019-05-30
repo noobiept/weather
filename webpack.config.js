2bb1dfad2ffdf6b55ef051b553a9670ac20f1fc7
Pedro Ferreira <plferreira4@gmail.com>
2019-05-30 15:19:25 +0100

    Update 'typescript' and the 'clean-webpack-plugin' dependencies to the latest versions.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CleanWebpackPlugin = require("clean-webpack-plugin");
+// @ts-ignore
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = function(env, argv) {
