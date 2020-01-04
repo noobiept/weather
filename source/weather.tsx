27b1e3e0218a80ceaed8b690198dde18aab11d99
Pedro Ferreira <plferreira4@gmail.com>
2020-01-04 13:44:26 +0100

    Convert the 'Weather' component into a functional react component.

--- source/weather.tsx
+++ source/weather.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useRef, useEffect, useState } from "react";
 
 import CityInput from "./city_input";
 import Message from "./message";
@@ -10,63 +10,38 @@ import Help from "./help";
 import { getFromStorage, saveToStorage } from "./data";
 import { getCurrentWeather, getCurrentForecast } from "./requests";
 
-interface WeatherProps {}
+export default function Weather() {
+    const searchLimit = 5; // maximum number of elements in the search list
+    const cityInputRef = useRef<HTMLInputElement>(null);
+    const loadedCities = getFromStorage("weather_search_list");
+    const loadedPosition = getFromStorage("weather_selected_position");
+    const [cities, setCities] = useState(loadedCities ? loadedCities : []); // list with all the city names that were searched for
+    const [position, setPosition] = useState(
+        typeof loadedPosition === "number" ? loadedPosition : -1
+    ); // position of the currently selected city name element
+    const [current, setCurrent] = useState();
+    const [forecast, setForecast] = useState();
+    const [messageText, setMessageText] = useState(""); // warn/error message to show to the user
+    const [loading, setLoading] = useState(false);
 
-interface WeatherState {
-    current: React.ReactNode | undefined;
-    forecast: React.ReactNode | undefined;
-    messageText: string; // warn/error message to show to the user
-    loading: boolean;
-    searchedCities: string[]; // list with all the city names that were searched for
-    searchedPosition: number; // position of the currently selected city name element
-}
-
-export default class Weather extends React.Component<
-    WeatherProps,
-    WeatherState
-> {
-    cityInputRef: React.RefObject<HTMLInputElement>;
-    searchLimit = 5; // maximum number of elements in the search list
-
-    constructor(props: WeatherProps) {
-        super(props);
-
-        const cities = getFromStorage("weather_search_list");
-        const position = getFromStorage("weather_selected_position");
-
-        this.cityInputRef = React.createRef();
-        this.changeCity = this.changeCity.bind(this);
-        this.state = {
-            current: undefined,
-            forecast: undefined,
-            messageText: "",
-            loading: false,
-            searchedCities: cities ? cities : [],
-            searchedPosition: typeof position === "number" ? position : -1,
-        };
-    }
-
-    componentDidMount() {
+    useEffect(() => {
         // focus the city element element whenever a key is pressed
         window.addEventListener("keypress", () => {
-            this.gainFocus();
+            gainFocus();
         });
 
         // load the last city that was selected in the previous session
-        const cities = this.state.searchedCities;
-        const position = this.state.searchedPosition;
-
         if (cities.length !== 0 && position >= 0) {
             const name = cities[position];
-            this.changeCity(name, position);
+            changeCity(name, position);
         }
-    }
+    }, []);
 
     /**
      * Put the focus on the `input` html element.
      */
-    gainFocus() {
-        const cityInput = this.cityInputRef.current;
+    function gainFocus() {
+        const cityInput = cityInputRef.current;
         if (cityInput) {
             cityInput.focus();
         }
@@ -76,15 +51,14 @@ export default class Weather extends React.Component<
      * Load a different city weather information.
      * If `existingPosition` is not given, then its a new city that we need to add to the cities list.
      */
-    async changeCity(name: string, existingPosition?: number) {
+    async function changeCity(name: string, existingPosition?: number) {
         if (name.length <= 3) {
-            this.setState({
-                messageText: "The query needs to have more than 3 characters.",
-            });
+            setMessageText("The query needs to have more than 3 characters.");
             return;
         }
 
-        this.setState({ loading: true, messageText: "" });
+        setLoading(true);
+        setMessageText("");
 
         try {
             var [current, forecast] = await Promise.all([
@@ -92,51 +66,41 @@ export default class Weather extends React.Component<
                 await getCurrentForecast(name),
             ]);
         } catch {
-            this.setState({
-                messageText: "Failed to connect to the weather API.",
-            });
+            setMessageText("Failed to connect to the weather API.");
             return;
         } finally {
-            this.setState({
-                loading: false,
-            });
+            setLoading(false);
         }
 
         if (current && forecast) {
             // a new city that we need to add
             if (typeof existingPosition === "undefined") {
-                existingPosition = this.addCityName(current.name);
+                existingPosition = addCityName(current.name);
             }
 
-            this.updateCityPosition(existingPosition);
-            this.setState({
-                current: (
-                    <CurrentWeather
-                        key={"current." + current.name}
-                        info={current}
-                    />
-                ),
-                forecast: (
-                    <Forecast
-                        key={"forecast." + forecast.city.name}
-                        info={forecast}
-                    />
-                ),
-            });
+            updateCityPosition(existingPosition);
+            setCurrent(
+                <CurrentWeather
+                    key={"current." + current.name}
+                    info={current}
+                />
+            );
+            setForecast(
+                <Forecast
+                    key={"forecast." + forecast.city.name}
+                    info={forecast}
+                />
+            );
         } else {
-            this.setState({
-                messageText: `Couldn't find a city with the name "${name}"`,
-            });
+            setMessageText(`Couldn't find a city with the name "${name}"`);
         }
     }
 
     /**
      * Set a new selected city position.
      */
-    updateCityPosition(position: number) {
-        this.setState({
-            searchedPosition: position,
-        });
+    function updateCityPosition(position: number) {
+        setPosition(position);
         saveToStorage("weather_selected_position", position);
     }
 
@@ -144,54 +108,46 @@ export default class Weather extends React.Component<
      * Add a new city to the search list.
      * Returns the position of the new entry in the search list.
      */
-    addCityName(name: string) {
-        const updated = this.state.searchedCities.slice();
+    function addCityName(name: string) {
+        const updated = cities.slice();
         let position = updated.length;
         updated.push(name);
 
         // if we get past the limit, remove the older entry (at the start of the array)
-        if (updated.length > this.searchLimit) {
+        if (updated.length > searchLimit) {
             updated.splice(0, 1);
             position--;
         }
 
-        this.setState({
-            searchedCities: updated,
-        });
-
+        setCities(updated);
         saveToStorage("weather_search_list", updated);
 
         return position;
     }
 
-    render() {
-        return (
-            <div>
-                <div className="list">
-                    <CityInput
-                        inputRef={this.cityInputRef}
-                        onInput={this.changeCity}
-                    />
-                    <Loading active={this.state.loading} />
-                </div>
-                <div>
-                    <Message text={this.state.messageText} />
-                </div>
-                <SearchList
-                    cityNames={this.state.searchedCities}
-                    selectedPosition={this.state.searchedPosition}
-                    onItemClick={this.changeCity}
-                />
-
-                {this.state.searchedCities.length !== 0 ? (
-                    <div id="WeatherInfoContainer">
-                        {this.state.current}
-                        {this.state.forecast}
-                    </div>
-                ) : (
-                    <Help />
-                )}
+    return (
+        <div>
+            <div className="list">
+                <CityInput inputRef={cityInputRef} onInput={changeCity} />
+                <Loading active={loading} />
             </div>
-        );
-    }
+            <div>
+                <Message text={messageText} />
+            </div>
+            <SearchList
+                cityNames={cities}
+                selectedPosition={position}
+                onItemClick={changeCity}
+            />
+
+            {cities.length !== 0 ? (
+                <div id="WeatherInfoContainer">
+                    {current}
+                    {forecast}
+                </div>
+            ) : (
+                <Help />
+            )}
+        </div>
+    );
 }
