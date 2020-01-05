1e8df9f8006931210de23226c5d008a4ebf92c18
Pedro Ferreira <plferreira4@gmail.com>
2020-01-05 01:18:25 +0100

    Start adding some tests to the project.
    - Configure the test framework (jest).
    - Add some tests for the 'Loading' component.
    - Run the tests on github's workflow.

---
+++ tests/components/loading.test.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import Loading from "../../source/components/loading";
+
+describe("Loading", () => {
+    test("Make sure it has all the elements.", () => {
+        const active = true;
+        const { container } = render(<Loading active={active} />);
+
+        const loading = container.querySelector(".loading");
+        expect(loading).toBeInTheDocument();
+
+        // should have the loading animation within
+        expect(loading.childElementCount).not.toBe(0);
+    });
+
+    test("Shouldn't have the animation when its not active.", () => {
+        const active = false;
+        const { container } = render(<Loading active={active} />);
+
+        const loading = container.querySelector(".loading");
+        expect(loading).toBeInTheDocument();
+        expect(loading.childElementCount).toBe(0);
+    });
+});
