9cb5080c115830e0862f5df927a4a21c489dbe20
Pedro Ferreira <plferreira4@gmail.com>
2020-01-05 14:13:57 +0100

    Add tests for the 'Wind' component.
    - Install a library to mock the canvas for testing purposes.

---
+++ tests/components/wind.test.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { render } from "@testing-library/react";
+import "@testing-library/jest-dom/extend-expect";
+
+import Wind from "../../source/components/wind";
+
+describe("Wind", () => {
+    test("That it has the right elements.", async () => {
+        const props = {
+            speed: 10,
+            degree: 90,
+            canvasWidth: 30,
+            canvasHeight: 40,
+        };
+        const { container } = render(<Wind {...props} />);
+
+        const element = container.querySelector("div");
+        const value = element.querySelector(".value");
+        const canvas = element.querySelector(
+            ".degreeCanvas"
+        ) as HTMLCanvasElement;
+
+        expect(element).toHaveTextContent("Wind Speed:");
+        expect(value).toHaveTextContent(props.speed.toString());
+        expect(element).toHaveTextContent("meter/sec");
+        expect(canvas).toBeInTheDocument();
+        expect(canvas.width).toBe(props.canvasWidth);
+        expect(canvas.height).toBe(props.canvasHeight);
+    });
+});
